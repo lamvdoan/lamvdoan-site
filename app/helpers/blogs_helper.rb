bc6c0dea98ffbb11c922d2e3d33f231357ed90e8
lamvdoan <lamvdoan@gmail.com>
2019-07-30 12:51:50 -0500

    styled blog form and refactored divs between blog files

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,21 @@
 module BlogsHelper
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+
+  def markdown text
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    settings = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      superscript: true
+    }
+
+    markdown = Redcarpet::Markdown.new(coderayified, settings)
+    markdown.render(text).html_safe
+  end
 end
