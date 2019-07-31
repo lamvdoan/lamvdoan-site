694b557b61ce2c77d9bda03846026a987381e264
lamvdoan <lamvdoan@gmail.com>
2019-07-31 12:07:14 -0500

    created topic model and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_200629) do
+ActiveRecord::Schema.define(version: 2019_07_31_163725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2019_07_30_200629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-15.times do |blog_count|
+14.times do |blog_count|
   Blog.create!(
-    title: "Title #{blog_count}",
+    title: "Blog #{blog_count + 1}",
     body: "This is the body",
     topic_id: 1
   )
 end
 
+Blog.create!(
+    title: "Blog 0",
+    body: "This is my 1st time!\r\nthis is the 2^(nd) time\r\n\r\nAlways wait for element to appear first before taking any action.\r\n\r\nfoo_bar_baz\r\n\r\nwww.google.com\r\nhttp://facebook.com\r\nLamvdoan@gmail.com\r\n\r\n```python\r\nYES_BUTTON = \"id=session_timeout_warning_button_yes\"\r\nselenium_helper.wait_until_page_contains_element(YES_BUTTON)\r\nselenium_helper.click_element(YES_BUTTON)\r\n```\r\n",
+    description: "Sample Python code\r\n\r\n```python\r\nYES_BUTTON = \"id=session_timeout_warning_button_yes\"\r\nselenium_helper.wait_until_page_contains_element(YES_BUTTON)\r\nselenium_helper.click_element(YES_BUTTON)\r\n```",
+    topic_id: 1
+  )
+
 puts "Logs: 15 Blogs created"
+
+
+5.times do |topic_count|
+  Topic.create!(
+    title: "Topic #{topic_count}",
+  )
+end
+
+puts "Logs: 5 Blogs created"
