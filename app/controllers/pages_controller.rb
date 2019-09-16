42fbc28d352764eff9d373a0a20297ee22c3e471
lamvdoan <lamvdoan@gmail.com>
2019-09-16 13:51:40 -0500

    displays on desktop but not mobile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,6 +16,8 @@ class PagesController < ApplicationController
   def resume
     @url = ENV.fetch("RESUME_URL")
     @title = @prefix + title_delimiter("Resume")
+    @url_pdf = "https://docs.google.com/document/d/1ILyR5rVdyX4kQiIoWqIaKXEF-6LqEOAEujnGpY7gUcE/export?format=pdf"
+    @url_publish = "https://docs.google.com/document/d/e/2PACX-1vQ3ZirMztSs8iPmhdjiDVJ2Q45H54neTHCzB6ECsgy5XoKmIk077viNp2M2vMTNY858zzdlGVmavH-i/pub?embedded=true"
   end
 
   private
